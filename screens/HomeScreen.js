c27f4eeeb25ad42c8f4947a8078d763feb4f3dfe
femkegoos <femke.goos.01@gmail.com>
2026-03-21 11:44:51 +0100

    Enhance HomeScreen layout: add search input and update title styling

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, TextInput, StyleSheet, Text, View } from 'react-native';
 import ProductCard from '../components/ProductCards';
 
 
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>Dit is een componenet</Text>
+      <Text style={styles.title}>Shop</Text>
+        <TextInput style={styles.searchInput} placeholder="Search for products..." />
       <ProductCard />
       <ProductCard />
       <ProductCard />
@@ -20,6 +21,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+    searchInput: {
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 8,
+        padding: 10,
+        margin: 16,
+        width: '90%',
+    },
+    title: {
+        marginTop: 64,
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 16,
+    },
 });
 
 export default HomeScreen;
